615bf5470d616f10cb40f8472f9a86b703fd1672
Alberto Aldana <albertoaldanar@hotmail.com>
2020-09-12 14:38:46 -0600

    Created user model and controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :accept_all_params
+  def all_users
+    @users = User.all
+    render json: {"users": @users}
+  end
+
+  def create_user
+    @user = User.create!(accept_all_params)
+    render json: {
+      "user": @user
+    }, status: :ok
+  end
+
+  private
+  def accept_all_params
+    params.permit!
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defautls: {format: :json} do
+    namespace :v1 do
+      get '/users' => 'users#all_users'
+      # post '/create_route' => 'route#create_route'
+      # post '/payments' => 'users#add_card'
+      # get '/listings' => 'rooms#your_listings'
+      # get '/users' => 'users#all_users'
+      # post '/create_movie' => 'movies#create'
+      # get '/movies' => 'movies#all_movies'
+      # post '/edit_movie' => 'movies#edit_movie'
+
+      # resources :rooms do
+      #   member do
+      #     get '/reservations' => 'reservations#reservations_by_room'
+      #   end
+      # end
+      # resources :reservations do
+      #   member do
+      #     post '/approve' => 'reservations#approve'
+      #     post '/decline' => 'reservations#decline'
+      #   end
+      # end
+    end
+  end
 end
